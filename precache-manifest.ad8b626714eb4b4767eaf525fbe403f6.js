963f0c70cf97bb742a26e8546337096e78a239a0
Samuel Ware <sam.r.ware@gmail.com>
2019-04-21 11:53:57 -0500

    Updates

---
+++ precache-manifest.ad8b626714eb4b4767eaf525fbe403f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4b127e5cc1c8021cc8",
+    "url": "/static/css/main.12f3547e.chunk.css"
+  },
+  {
+    "revision": "da4b127e5cc1c8021cc8",
+    "url": "/static/js/main.a31ef4f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcc7c4a2f98b0a5b769b",
+    "url": "/static/css/2.0136ec20.chunk.css"
+  },
+  {
+    "revision": "dcc7c4a2f98b0a5b769b",
+    "url": "/static/js/2.9e1dce4f.chunk.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "4a52c3e0a1a7f29e96cfce1cb4ab378a",
+    "url": "/index.html"
+  }
+];
